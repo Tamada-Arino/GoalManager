b43bab938f567f4a6ef6183eb7ecbff3f368d584
xaiBUh29wX <arino.tamada@luxiar.com>
2024-07-22 15:20:56 +0900

    variantメソッドを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
 
-  has_one_attached :avatar
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
 end
