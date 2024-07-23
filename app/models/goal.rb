b8dc5485eb58b9739dd4d95ed9452b75967989d1
xaiBUh29wX <arino.tamada@luxiar.com>
2024-07-23 10:03:02 +0900

    モデルファイルを修正

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,10 +1,14 @@
 class Goal < ApplicationRecord
   validates :title, presence: true
   validate :start_end_check
-  validate :validate_date_order(:schedules_end_date)
-  validate :validate_date_order(:end_date)
+  validate :start_date_check
 
   private
+
+  def start_date_check
+    validate_date_order(:schedules_end_date)
+    validate_date_order(:end_date)
+  end
 
   def validate_date_order(target_date)
     if target_date.present? && self.start_date > self.target_date
