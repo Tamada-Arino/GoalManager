e16fe3f30ca91a3aac74ca6171d29727b249d264
xaiBUh29wX <arino.tamada@luxiar.com>
2024-07-23 09:34:53 +0900

    バリデーションを作成

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,2 +1,14 @@
 class Goal < ApplicationRecord
+  validates :title, presence: true
+  validate :start_end_check
+  validate :validate_date_order(:schedules_end_date)
+  validate :validate_date_order(:end_date)
+
+  private
+
+  def validate_date_order(target_date)
+    if target_date.present? && self.start_date > self.target_date
+      errors.add(target_date, :start_date_invalid)
+    end
+  end
 end
