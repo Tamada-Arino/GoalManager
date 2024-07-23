37f017a03a3f406f505455f34b30b7fad058d320
xaiBUh29wX <arino.tamada@luxiar.com>
2024-07-23 13:08:55 +0900

    newアクションとcreateアクションを追加

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -4,4 +4,10 @@ class GoalsController < ApplicationController
   def index
     @goals = current_user.goals.order(:created_at).page(params[:page])
   end
+
+  def new
+  end
+
+  def create
+  end
 end
