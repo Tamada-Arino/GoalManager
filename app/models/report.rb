a2271725f1866e6ad2e8611b95b2feaabc96f1b6
xaiBUh29wX <arino.tamada@luxiar.com>
2024-07-24 13:55:47 +0900

    バリデーションを作成

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,17 @@
 class Report < ApplicationRecord
+  validates :target_date, presence: true
+  validates :progress_value, presence: true, numericality: { in: 0..100 }
+  validate :progress_date_check
+
   belongs_to :goal
+
+  private
+
+  def progress_date_check
+    if target_date < goal.start_date
+      errors.add(:target_date, :start_date_invalid)
+    elsif goal.end_date.present? && target_date > goal.end_date
+      errors.add(:target_date, :end_date_invalid)
+    end
+  end
 end
