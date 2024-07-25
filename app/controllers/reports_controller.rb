1cda808779927142ef35d56f968f7ea3fe679fe2
xaiBUh29wX <arino.tamada@luxiar.com>
2024-07-25 09:34:01 +0900

    createアクションを実装

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,5 +5,18 @@ class ReportsController < ApplicationController
   end
 
   def create
+    @goal = current_user.goals.find(params[:goal_id])
+    @report = @goal.reports.new(report_params)
+    if @report.save
+      redirect_to @goal, notice: t('notice.create', content: Report.model_name.human)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(%i[target_date progress_value content])
   end
 end
