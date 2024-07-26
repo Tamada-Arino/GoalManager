ed279f19bb35f0513cc7d3f84c8bc63417ae491c
xaiBUh29wX <arino.tamada@luxiar.com>
2024-07-26 15:02:36 +0900

    コントローラーの値をbefore_actionで設定

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,18 +1,17 @@
 # frozen_string_literal: true
 
 class ReportsController < ApplicationController
+  before_action :set_goal
+
   def new
-    @goal = current_user.goals.find(params[:goal_id])
     @report = @goal.reports.new
   end
 
   def edit
-    @goal = current_user.goals.find(params[:goal_id])
     @report = @goal.reports.find(params[:id])
   end
 
   def create
-    @goal = current_user.goals.find(params[:goal_id])
     @report = @goal.reports.new(report_params)
     if @report.save
       redirect_to @goal, notice: t('notice.create', content: Report.model_name.human)
@@ -22,7 +21,6 @@ class ReportsController < ApplicationController
   end
 
   def update
-    @goal = current_user.goals.find(params[:goal_id])
     @report = @goal.reports.find(params[:id])
     if @report.update(report_params)
       redirect_to @goal, notice: t('notice.update', content: Report.model_name.human)
@@ -32,13 +30,16 @@ class ReportsController < ApplicationController
   end
 
   def destroy
-    @goal = current_user.goals.find(params[:goal_id])
     @report = @goal.reports.find(params[:id])
     @report.destroy
     redirect_to @goal, notice: t('notice.destroy', content: Report.model_name.human)
   end
 
   private
+
+  def set_goal
+    @goal = current_user.goals.find(params[:goal_id])
+  end
 
   def report_params
     params.require(:report).permit(%i[target_date progress_value content])
