9f8732010a9cbb18acd118d84160c4dbc6886568
xaiBUh29wX <arino.tamada@luxiar.com>
2024-07-30 18:11:00 +0900

    rubocopの自動修正を実行

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -7,9 +7,9 @@ class Calendar
     @range_last_date = @goal.end_date || Time.zone.today
     @range_start_date = [@range_last_date - @range, @goal.start_date].max
     @target_reports = @goal.reports
-                          .where(target_date: @range_start_date..range_last_date)
-                          .select(:target_date, :progress_value)
-                          .index_by(&:target_date)
+                           .where(target_date: @range_start_date..range_last_date)
+                           .select(:target_date, :progress_value)
+                           .index_by(&:target_date)
   end
 
   def generate_calendar
@@ -28,12 +28,9 @@ class Calendar
   end
 
   def generate_line
-    calendar = []
-    (@range_start_date..@range_last_date).each do |date|
-      calendar << target_date_class_and_style(@target_reports[date]&.progress_value)
+    (@range_start_date..@range_last_date).map do |date|
+      target_date_class_and_style(@target_reports[date]&.progress_value)
     end
-
-    calendar
   end
 
   private
@@ -43,7 +40,7 @@ class Calendar
   def target_date_class_and_style(progress_value)
     class_with_style = { class: 'date_cell' }
     if progress_value.present?
-      status_number = (progress_value - 1) / 25 + 1
+      status_number = ((progress_value - 1) / 25) + 1
       class_with_style[:class] += " progress_#{status_number}"
       class_with_style[:style] = "background-color: #{@goal.color};"
     end
