c351035e26a2542be55cd48dd9692fd137898228
xaiBUh29wX <arino.tamada@luxiar.com>
2024-08-01 15:42:39 +0900

    小目標のバリデーション追加

--- app/models/small_goal.rb
+++ app/models/small_goal.rb
@@ -1,4 +1,12 @@
 class SmallGoal < ApplicationRecord
+  validates :title, presence: true
+
   belongs_to :goal
   has_many :reports
+
+  def validate_small_goal_count
+    if goal.small_goals.count >= 3
+      errors.add(:base, :over_small_goals_count)
+    end
+  end
 end
