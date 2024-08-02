74299d72436242b413ae831f0b3328beaf70252e
xaiBUh29wX <arino.tamada@luxiar.com>
2024-08-02 17:41:28 +0900

    stimulusファイルの記述を変更。フォームの不必要な記述を削除

--- app/javascript/controllers/slider_controller.js
+++ app/javascript/controllers/slider_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["value"]
 
-  changeValue() {
-    this.valueTarget.textContent = document.querySelector("#slider").value
+  changeValue(event) {
+    this.valueTarget.textContent = event.target.value
   }
 }
