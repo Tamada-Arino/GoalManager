4c4da4fa05587f63ec7cc480c55aa29bb0034e7c
xaiBUh29wX <arino.tamada@luxiar.com>
2024-08-02 15:53:20 +0900

    stimulusファイルを作成

--- app/javascript/controllers/slider_controller.js
+++ app/javascript/controllers/slider_controller.js
@@ -1,7 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="slider"
 export default class extends Controller {
+  static targets = ["slider", "value"]
+
   connect() {
+    this.sliderTarget.addEventListener('input', () => this.changeValue())
+  }
+
+  changeValue() {
+    this.valueTarget.textContent = this.sliderTarget.value
   }
 }
