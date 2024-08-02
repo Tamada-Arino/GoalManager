23f3c28e169dfcd7c51205ca52952b88ea4c81d8
xaiBUh29wX <arino.tamada@luxiar.com>
2024-08-02 16:36:18 +0900

    stimulusのactionで発火するように変更

--- app/javascript/controllers/slider_controller.js
+++ app/javascript/controllers/slider_controller.js
@@ -3,10 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["slider", "value"]
 
-  connect() {
-    this.sliderTarget.addEventListener('input', () => this.changeValue())
-  }
-
   changeValue() {
     this.valueTarget.textContent = this.sliderTarget.value
   }
