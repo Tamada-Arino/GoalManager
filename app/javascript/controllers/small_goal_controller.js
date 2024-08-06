14ac0564d4e982860ee76103a4e879a91e27b644
xaiBUh29wX <arino.tamada@luxiar.com>
2024-08-06 16:19:19 +0900

    stimulusファイル仮実装

--- app/javascript/controllers/small_goal_controller.js
+++ app/javascript/controllers/small_goal_controller.js
@@ -1,7 +1,14 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="small-goal"
 export default class extends Controller {
+
   connect() {
+    console.log("読み取りOK");
   }
+
+  test() {
+    console.log("クリックしました");
+  }
+
 }
