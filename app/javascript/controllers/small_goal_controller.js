4a9f2f921f4f33c59efb8f25de4fa3c075dc94b9
xaiBUh29wX <arino.tamada@luxiar.com>
2024-08-06 17:52:59 +0900

    フォームを追加するstimulusファイル仮実装

--- app/javascript/controllers/small_goal_controller.js
+++ app/javascript/controllers/small_goal_controller.js
@@ -2,13 +2,26 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="small-goal"
 export default class extends Controller {
+  static targets = ["forms"]
 
   connect() {
-    console.log("読み取りOK");
+    this.index = 0;
   }
 
-  test() {
-    console.log("クリックしました");
+  insertHtml() {
+    const html = `
+      <div class="form small_goals">
+        <tr>
+          <th>小目標</th>
+          <td>
+            <input type="text" name="goal[small_goals_attributes][${this.index}][title]" />
+            <input type="checkbox" name="goal[small_goals_attributes][${this.index}][achievable]" />
+            <label for="goal_small_goals_attributes_${this.index}_achievable">達成済み</label>
+          </td>
+        </tr>
+      </div>
+    `;
+    this.formsTarget.insertAdjacentHTML('beforeend', html);
+    this.index++;
   }
-
 }
