232efb67dfa86c33b4782995650bf674ac1e17fa
xaiBUh29wX <arino.tamada@luxiar.com>
2024-08-07 12:15:25 +0900

    小目標のバリデーション変更

--- app/models/small_goal.rb
+++ app/models/small_goal.rb
@@ -6,7 +6,8 @@ class SmallGoal < ApplicationRecord
 
   def validate_small_goal_count
     if goal.small_goals.count >= 3
-      errors.add(:base, :over_small_goals_count)
+      goal.erros.add(:base, :over_small_goals_count)
+      raise ActiveRecord::RecordInvalid
     end
   end
 end
