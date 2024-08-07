5a3a14ac403f34e1e77ca44e6fd1ae0b33a0bdd5
xaiBUh29wX <arino.tamada@luxiar.com>
2024-08-07 17:16:47 +0900

    不要な記述を削除

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -11,7 +11,7 @@ class Goal < ApplicationRecord
 
   belongs_to :user
   has_many :reports, dependent: :destroy
-  has_many :small_goals, dependent: :destroy, inverse_of: :goal
+  has_many :small_goals, dependent: :destroy
 
   def status
     if end_date.present?
