69d8a05b34daff4e48aa4e2d5a1e4cec86c8403d
xaiBUh29wX <arino.tamada@luxiar.com>
2024-08-07 10:00:11 +0900

    小目標の削除機能を実装

--- app/javascript/controllers/small_goal_controller.js
+++ app/javascript/controllers/small_goal_controller.js
@@ -11,17 +11,19 @@ export default class extends Controller {
   insertHtml() {
     const html = `
       <div class="form small_goals">
-        <tr>
-          <th>小目標</th>
-          <td>
-            <input type="text" name="goal[small_goals_attributes][${this.index}][title]" />
-            <input type="checkbox" name="goal[small_goals_attributes][${this.index}][achievable]" />
-            <label for="goal_small_goals_attributes_${this.index}_achievable">達成済み</label>
-          </td>
-        </tr>
+        <label>小目標</label>
+        <br>
+        <input type="text" name="goal[small_goals_attributes][${this.index}][title]" />
+        <input type="checkbox" name="goal[small_goals_attributes][${this.index}][achievable]" />
+        <label for="goal_small_goals_attributes_${this.index}_achievable">達成済み</label>
+        <button type="button" data-action="click->small-goal#removeHtml">削除</button>
       </div>
     `;
     this.formsTarget.insertAdjacentHTML('beforeend', html);
     this.index++;
+  }
+
+  removeHtml(event) {
+    event.currentTarget.closest("div").remove();
   }
 }
