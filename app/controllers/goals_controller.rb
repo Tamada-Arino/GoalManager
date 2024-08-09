1190733a90910f110a4e49b1515b43c0a9ad46d9
xaiBUh29wX <arino.tamada@luxiar.com>
2024-08-09 10:14:30 +0900

    updateアクション周りのリファクタリング

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -39,24 +39,19 @@ class GoalsController < ApplicationController
     end
   end
 
-  # rubocop:disable Metrics/AbcSize
-  # rubocop:disable Metrics/MethodLength
   def update
-    small_goals_attributes = params.dig(:goal, :small_goals_attributes)
     @goal.assign_attributes(goal_params)
 
     ActiveRecord::Base.transaction do
-      destroy_small_goals(@goal, small_goals_attributes)
-      update_or_create_small_goals(@goal, small_goals_attributes)
+      destroy_small_goals
+      update_or_create_small_goals
       @goal.save!
     end
     redirect_to @goal, notice: t('notice.update', content: Goal.model_name.human)
   rescue ActiveRecord::RecordInvalid => e
-    move_small_goals_error(@goal, e)
+    move_small_goals_error(e)
     render :edit, status: :unprocessable_entity
   end
-  # rubocop:enable Metrics/AbcSize
-  # rubocop:enable Metrics/MethodLength
 
   def destroy
     @goal.destroy
@@ -84,33 +79,37 @@ class GoalsController < ApplicationController
     end
   end
 
-  def destroy_small_goals(goal, small_goals_attributes)
-    entered_small_goals = small_goals_attributes&.values&.pluck(:id)&.map(&:to_i) || []
-
-    goal.small_goals.where.not(id: entered_small_goals).destroy_all
+  def small_goals_attributes
+    @small_goals_attributes ||= params.dig(:goal, :small_goals_attributes)
   end
 
-  def update_or_create_small_goals(goal, small_goals_attributes)
+  def destroy_small_goals
+    entered_small_goals = small_goals_attributes&.values&.pluck(:id)&.map(&:to_i) || []
+
+    @goal.small_goals.where.not(id: entered_small_goals).destroy_all
+  end
+
+  def update_or_create_small_goals
     return if small_goals_attributes.blank?
 
     small_goals_attributes.each_value do |small_goal_params|
       if small_goal_params.key?(:id)
-        update_small_goals(goal, small_goal_params)
+        update_small_goals(small_goal_params)
       else
-        create_small_goals(goal, small_goal_params)
+        create_small_goals(small_goal_params)
       end
     end
   end
 
-  def create_small_goals(goal, small_goal_params)
-    goal.small_goals.create!(
+  def create_small_goals(small_goal_params)
+    @goal.small_goals.create!(
       title: small_goal_params[:title],
       achievable: small_goal_params[:achievable]
     )
   end
 
-  def update_small_goals(goal, small_goal_params)
-    small_goal = goal.small_goals.find(small_goal_params[:id].to_i)
+  def update_small_goals(small_goal_params)
+    small_goal = @goal.small_goals.find(small_goal_params[:id].to_i)
     small_goal.update!(
       title: small_goal_params[:title],
       achievable: small_goal_params[:achievable]
