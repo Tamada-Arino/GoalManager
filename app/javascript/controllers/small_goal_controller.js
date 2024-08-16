1f3a3206e06516ddc90e953fb5dfc7f367836418
xaiBUh29wX <arino.tamada@luxiar.com>
2024-08-16 12:04:28 +0900

    小目標入力フォームのスタイル変更

--- app/javascript/controllers/small_goal_controller.js
+++ app/javascript/controllers/small_goal_controller.js
@@ -14,7 +14,7 @@ export default class extends Controller {
       <div class="control small_goals mx-3">
         <label>小目標</label>
         <br>
-        <input type="text" name="goal[small_goals_attributes][${this.index}][title]" />
+        <input type="text" class="input is-small" name="goal[small_goals_attributes][${this.index}][title]">
         <button type="button" class="button is-danger is-light is-small" data-action="click->small-goal#removeHtml">
           削除
         </button>
