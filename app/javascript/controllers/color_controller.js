b74a9f8c6a8126f74a198baf5eeb74357bd97d96
xaiBUh29wX <arino.tamada@luxiar.com>
2024-08-19 09:36:05 +0900

    ターゲット名を変更

--- app/javascript/controllers/color_controller.js
+++ app/javascript/controllers/color_controller.js
@@ -2,15 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="color"
 export default class extends Controller {
-  static targets = ["radioButton", "customColor"]
+  static targets = ["customRadio", "customColor"]
 
   connect() {
     this.initializeColor()
   }
 
   changeColorValue(event) {
-    this.radioButtonTarget.value = event.target.value
-    if (this.radioButtonTarget.checked) {
+    this.customRadioTarget.value = event.target.value
+
+    if (this.customRadioTarget.checked) {
       this.changeSample(event);
     }
   }
