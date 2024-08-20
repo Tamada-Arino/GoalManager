e8d17dd44d8686829e08f52aadf13c2e04ea7998
xaiBUh29wX <arino.tamada@luxiar.com>
2024-08-20 13:26:06 +0900

    大目標フォームのstimulusファイルの関数切り出し

--- app/javascript/controllers/color_controller.js
+++ app/javascript/controllers/color_controller.js
@@ -1,8 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
+import { CellStyles } from "./cell_styles"
 
 // Connects to data-controller="color"
 export default class extends Controller {
   static targets = ["customRadio", "customColor"]
+
+  applyStyles = CellStyles.applyStyles
+  darkenColorValue = CellStyles.darkenColorValue
 
   connect() {
     this.initializeColor()
@@ -41,24 +45,5 @@ export default class extends Controller {
       nomal: document.querySelector("#sample_3"),
       good: document.querySelector("#sample_4")
     }
-  }
-
-  applyStyles(cell, color, opacity = 1) {
-    cell.style.backgroundColor = color
-    cell.style.opacity = opacity
-  }
-
-  darkenColorValue(hex) {
-    const darkenValue = 65
-
-    const rgb = [
-      parseInt(hex.slice(1, 3), 16),
-      parseInt(hex.slice(3, 5), 16),
-      parseInt(hex.slice(5, 7), 16)
-    ]
-
-    const darkenedRgb = rgb.map(value => Math.max(0, value - darkenValue))
-
-    return `rgb(${darkenedRgb.join(", ")})`
   }
 }
