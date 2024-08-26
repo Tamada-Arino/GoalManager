d90ed16bbbf32e3d9e30b897d22b1b0abaa5bb6c
xaiBUh29wX <arino.tamada@luxiar.com>
2024-08-26 15:20:20 +0900

    Userのモデルスペックのdescribeとitを作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー登録' do
+    it 'ユーザー名、メールアドレス、パスワード、パスワード（確認用）が存在すれば登録できること' do
+
+    end
+
+    it 'ユーザー名が空白の時登録できないこと' do
+
+    end
+
+    it 'メールアドレスが空白の時登録できないこと' do
+
+    end
+
+    it 'パスワードが空白の時登録できないこと' do
+
+    end
+
+    it 'パスワード（確認用）が空白の時登録できないこと' do
+
+    end
+
+    it 'パスワードとパスワード（確認用）が違う文字列の場合登録できないこと' do
+
+    end
+  end
 end
