afd121263f93a7576703825c7f8012e2c5084bb6
xaiBUh29wX <arino.tamada@luxiar.com>
2024-08-26 15:46:46 +0900

    user_spec作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,27 +3,33 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'ユーザー登録' do
     it 'ユーザー名、メールアドレス、パスワード、パスワード（確認用）が存在すれば登録できること' do
-
+      user = build(:user)
+      expect(user).to be_valid
     end
 
     it 'ユーザー名が空白の時登録できないこと' do
-
+      user = build(:user, name: '')
+      expect(user).not_to be_valid
     end
 
     it 'メールアドレスが空白の時登録できないこと' do
-
+      user = build(:user, email: '')
+      expect(user).not_to be_valid
     end
 
     it 'パスワードが空白の時登録できないこと' do
-
+      user = build(:user, password: '')
+      expect(user).not_to be_valid
     end
 
     it 'パスワード（確認用）が空白の時登録できないこと' do
-
+      user = build(:user, password_confirmation: '')
+      expect(user).not_to be_valid
     end
 
     it 'パスワードとパスワード（確認用）が違う文字列の場合登録できないこと' do
-
+      user = build(:user, password_confirmation: 'another_password')
+      expect(user).not_to be_valid
     end
   end
 end
