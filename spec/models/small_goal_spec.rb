c177ab43a15721fb71ab9042c41ddf75b897faed
xaiBUh29wX <arino.tamada@luxiar.com>
2024-08-27 14:32:05 +0900

    small_goalのモデルスペック作成

--- spec/models/small_goal_spec.rb
+++ spec/models/small_goal_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe SmallGoal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '小目標登録' do
+    it '小目標タイトルと達成状況があれば登録できること' do
+      small_goal = build(:small_goal)
+      expect(small_goal).to be_valid
+    end
+
+    it '小目標タイトルがなければ登録できないこと' do
+      small_goal = build(:small_goal, title: '')
+      expect(small_goal).not_to be_valid
+    end
+  end
 end
